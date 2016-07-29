3523829cd48ffaeefbe73256f80ebc14b5274ab0
Faraz Patankar <farazpatankar@gmail.com>
2016-07-29 18:45:29 +0530

    refactor response generation into a helper method

--- app/helpers/slack_api_helper.rb
+++ app/helpers/slack_api_helper.rb
@@ -1,2 +1,11 @@
 module SlackApiHelper
+	def response_generator(line)
+		response = ""
+
+		line.users.each_with_index do |el, i|
+			response += "#{i + 1}. #{el.user_name}\n"
+		end
+
+		response
+	end
 end
