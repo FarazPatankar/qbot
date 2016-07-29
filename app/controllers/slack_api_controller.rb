75711dee53a36fa72da67b89c9f02384372402b2
Faraz Patankar <farazpatankar@gmail.com>
2016-07-29 17:10:08 +0530

    set up basic functionality

--- app/controllers/slack_api_controller.rb
+++ app/controllers/slack_api_controller.rb
@@ -1,8 +1,25 @@
 class SlackApiController < ApplicationController
 	def handle_message
+		channel_id = params[:channel_id]
+		channel_name = params[:channel_name]
 
-		puts params.inspect
+		user_id = params[:user_id]
+		user_name = params[:user_name]
 
-		render:json => "Hello"
+		line = Line.where(channel_id: channel_id).first
+
+		if line.nil?
+			line = Line.create(channel_id: channel_id, channel_name: channel_name)
+		end
+
+		user = User.where(user_id: user_id).first
+
+		if user.nil?
+			user = User.create(user_id: user_id, user_name: user_name)
+		end
+
+		line.users.push(user)
+
+		render :json => "You are number #{line.users.count} in the queue"
 	end
 end
