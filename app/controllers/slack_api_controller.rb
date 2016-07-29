84ef501cf359909aab1101123d9211f0f372dd8b
Faraz Patankar <farazpatankar@gmail.com>
2016-07-29 22:06:27 +0530

    add responses for help, view and pop

--- app/controllers/slack_api_controller.rb
+++ app/controllers/slack_api_controller.rb
@@ -8,29 +8,72 @@ class SlackApiController < ApplicationController
 		user_id = params[:user_id]
 		user_name = params[:user_name]
 
-		line = Line.where(channel_id: channel_id).first
+		text = params[:text]
 
-		if line.nil?
-			line = Line.create(channel_id: channel_id, channel_name: channel_name)
+		if text == "help"
+
+			if line.nil?
+				line = Line.create(channel_id: channel_id, channel_name: channel_name)
+			end
+
+			if user.nil?
+				user = User.create(user_id: user_id, user_name: user_name)
+			end
+
+			line.users.push(user)
+
+			response = response_generator(line)
+
+			render :json => {
+							    "text": "The queue now is :",
+							    "attachments": [
+							        {
+							            "text": response
+							        }
+							    ]
+							}
+		elsif text == "view"
+			line = Line.where(channel_id: channel_id).first
+
+			if line.nil?
+				line = Line.create(channel_id: channel_id, channel_name: channel_name)
+			end
+
+			response = response_generator(line)
+
+			render :json => {
+							    "text": "The queue now is :",
+							    "attachments": [
+							        {
+							            "text": response.empty? ? "EMPTY" : response
+							        }
+							    ]
+							}
+
+		elsif text == "pop"
+			line = Line.where(channel_id: channel_id).first
+
+			if line.users.count > 0
+				popped_user = line.users.first.user_name
+
+				line.users.first.destroy
+
+				response = response_generator(line)
+
+				render :json => {
+							    "text": "You popped the user - #{popped_user}. The queue now is :",
+							    "attachments": [
+							        {
+							            "text": response.empty? ? "EMPTY" : response
+							        }
+							    ]
+							}
+			else
+				render :json => "There's nothing to pop, son!"
+			end
+
+		else
+			render :json => "I don't get it. Perhaps you should check the documentation, son."
 		end
-
-		user = User.where(user_id: user_id).first
-
-		if user.nil?
-			user = User.create(user_id: user_id, user_name: user_name)
-		end
-
-		line.users.push(user)
-
-		response = response_generator(line)
-
-		render :json => {
-						    "text": "The queue now is :",
-						    "attachments": [
-						        {
-						            "text": response
-						        }
-						    ]
-						}
 	end
 end
